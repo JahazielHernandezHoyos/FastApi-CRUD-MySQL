e74453d76220c4b56afed7673c394e17f677a316
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2022-06-23 09:08:19 -0500

    cambios

--- peticiones.py
+++ peticiones.py
@@ -2,7 +2,14 @@ import requests
 import json
 
 # mandar en el header "Hola" a la url "http:127.0.0.1:8000/headers"
-headers = {'Hola': '12312312312132'}
+
+
+headers = {
+    'Hola': '12312312312132'}
+
+
 r = requests.get('http://127.0.0.1:8000/headers', headers=headers)
+
+
 print(r.text)
 
